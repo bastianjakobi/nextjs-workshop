cedd82b36ea79065d6c49c8e5153a9e052a23eb3
Bastian Jakobi <>
2024-05-07 11:49:26 +0200

    feat: add files for React Basics project

---
+++ react-basics/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React Basics</h1>
+      <p>
+        {count}
+      </p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  )
+}
+
+export default App
